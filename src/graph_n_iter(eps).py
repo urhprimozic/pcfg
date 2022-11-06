c6e883218c850298a45a3870d3080310ecc9da99
urh <urh.primozic@gmail.com>
2022-11-06 21:28:09 +0100

    different values of p

--- src/graph_n_iter(eps).py
+++ src/graph_n_iter(eps).py
@@ -3,14 +3,16 @@ from linear import n_iter
 from utils import eq_qs
 import numpy as np
 
-p = 0.5
-qs = eq_qs(10)
-
+ps = [0.2, 0.5, 0.8]
+k = 10
 epsilons = np.linspace(1e-8, 0.1, 1500)
-y = [n_iter(eps, p, *qs) for eps in epsilons]
-plt.plot(epsilons, y)
+for p in ps:
+    qs = eq_qs(k)
+    y = [n_iter(eps, p, *qs) for eps in epsilons]
+    plt.plot(epsilons, y, label=f'p={p}')
 plt.title('Number of iterations with respect to epsilon')
 plt.xlabel('Epsilon')
+plt.legend()
 plt.ylabel('Number of iterations')
 plt.savefig('../img/n_iter(time).png')
 plt.show()
